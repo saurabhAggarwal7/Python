7a1baa3dbe080d554fd5381c9694f2b7a5ba6bda
Saurabh Aggarwal <sorav77@gmail.com>
2020-10-07 03:24:42 -0700

    commit

---
+++ Inheritence/Lambda/Parent.py
@@ -0,0 +1,7 @@
+class Parent(object):
+    def __init__(self, **kwargs):
+        self.owner = kwargs.get('owner')
+        
+    def print_parent_status(self, payload_json):
+        print(f'In Parent Class payload json recieved is {payload_json}')
+        print(f'In Parent Class owner is {self.owner}')
